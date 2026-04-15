c56434b4117ef72e6bbeb4eaa68fa95328e8beee
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-04-15 12:15:16 -0400

    Added test for empty namespaces for Husky A300 to match clearpath_config.

---
+++ clearpath_generator_common_tests/samples/test_a300_empty_ns/setup.bash
@@ -0,0 +1,9 @@
+# Bash setup generated at 2026-04-15 16:10:06.995995+00:00
+# Core ROS setup
+source /opt/ros/jazzy/setup.bash
+# ROS configuration
+export ROS_DOMAIN_ID="0"
+export RMW_IMPLEMENTATION="rmw_fastrtps_cpp"
+unset ROS_DISCOVERY_SERVER
+export ROS_AUTOMATIC_DISCOVERY_RANGE="SUBNET"
+unset ROS_STATIC_PEERS
